f9ab60aabe79a241818a8d9028d7b2b70b67bad8
Peter Moresi <peter.moresi@gmail.com>
2015-12-07 21:01:00 -0800

    add neat and react-remarkable

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,10 +1,24 @@
 import React, { PropTypes } from 'react'
+import Markdown from 'react-remarkable'
 
+let content = `
+## Do we need FAQs?
+
+Yes
+
+## Will this product be awesome?
+
+Yes
+
+| test | test |
+|-|-|
+| does | this work |
+`
 const FAQs = React.createClass({
   render () {
     return (
       <div id="faqs">
-        Put FAQs here
+        <Markdown source={content} />
       </div>
     )
   }
