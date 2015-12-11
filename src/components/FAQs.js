41abe8057a0dc6028a20167b84f913d781aee8df
Peter Moresi <peter.moresi@gmail.com>
2015-12-10 21:02:46 -0800

    Extract markdown options

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import Markdown from 'react-remarkable'
-import hljs from 'highlight.js' // https://highlightjs.org/
+import markdownOptions from './markdown-options'
 
 var content = require('../content/FAQs.md');
 
@@ -10,22 +10,7 @@ const FAQs = React.createClass({
       <div id="faqs">
         <Markdown
           source={ content }
-          options={ {
-            highlight: function (str, lang) {
-              console.log(str, lang)
-              if (lang && hljs.getLanguage(lang)) {
-                try {
-                  return hljs.highlight(lang, str).value;
-                } catch (err) {}
-              }
-
-              try {
-                return hljs.highlightAuto(str).value;
-              } catch (err) {}
-
-              return ''; // use external default escaping
-            }
-          }}
+          options={ markdownOptions }
           />
       </div>
     )
---
+++ src/components/markdown-options.js
@@ -0,0 +1,18 @@
+import hljs from 'highlight.js' // https://highlightjs.org/
+
+export default {
+  highlight: function (str, lang) {
+    console.log(str, lang)
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) {}
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) {}
+
+    return ''; // use external default escaping
+  }
+}
