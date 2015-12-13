1cce4a172b19aa73636af3635c7361daa17fe34d
Sean King <spking82@gmail.com>
2015-12-13 10:27:28 -0800

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,12 @@ const Footer = React.createClass({
   render () {
     return (
       <div className="footer">
+      <p>
         &copy; 2015 FormBucket <Link to="faqs">FAQs</Link> | <Link to="api">API</Link>
+      </p>
+      <p>
+        Forged in California by <Link to="http://functionfoundry.com">FunctionFoundry</Link>
+      </p>
       </div>
     )
   }
