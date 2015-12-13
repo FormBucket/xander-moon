c865bb01fca2c2e45abbe2d910f1cafe817e3a7e
Sean King <spking82@gmail.com>
2015-12-13 10:06:16 -0800

    updating signup page to include email option

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -19,6 +19,21 @@ const Signup = React.createClass({
             <button onClick={redirect('auth/google')}><FontAwesome name='google' /> Sign Up with Google</button>
             <button onClick={redirect('auth/github')}><FontAwesome name='github' /> Sign Up with GitHub</button>
           </div>
+          <div className="or">
+            <p>OR</p>
+          </div>
+          <div className="email-signup">
+            <p>Sign up with Email</p>
+              <label for="fullName">Full Name</label>
+              <input type="text" id="fullName" placeholder="e.g. Nikola Tesla"/>
+              <label for="emailAddress">Email Address</label>
+              <input type="text" id="emailAddress" placeholder="nikola@altcurrent.com"/>
+              <label for="createPassword"><FontAwesome name='lock' /> Create Password</label>
+              <input type="password" id="createPassword"/>
+              <label for="confirmPassword"><FontAwesome name='lock' /> Confirm Password</label>
+              <input type="password" id="confirmPassword"/>
+              <button onClick={redirect('')}>Create Account</button>
+          </div>
         </div>
       </div>
     )
