1faf98532ef9304d2646afccae04f212fac29862
Peter Moresi <peter.moresi@gmail.com>
2015-12-16 08:07:36 -0800

    Add initial version of webutils

---
+++ src/utils/webutils.js
@@ -0,0 +1,81 @@
+/* A collection of functions to send requests to the server.
+Author: Peter Moresi
+Date: 2015-12-14
+
+All of these functions return a promise.
+*/
+
+// generic function to detect common HTTP error codes. Credit to Mozilla.
+export function processStatus(response) {
+  // status "0" to handle local files fetching (e.g. Cordova/Phonegap etc.)
+  if (response.status === 200 || response.status === 0) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+/* Send server request to get user's Forms
+
+  Usage:
+  createForm({
+    name: 'test2', enabled: true, email_to: 'test@test8.com', webhooks: [], required_fields: []
+  })
+*/
+export function getForms(data){
+  return fetch('/forms.json', {
+    credentials: 'include',
+    method: 'get'
+  })
+}
+
+/* Send server request to get a specific Forms
+
+  Usage:
+  createForm({
+    name: 'test2', enabled: true, email_to: 'test@test8.com', webhooks: [], required_fields: []
+  })
+*/
+export function getForm(id){
+  return fetch(`/form/${id}.json`, {
+    credentials: 'include',
+    method: 'get'
+  })
+}
+
+/* Send server request to create new form
+
+  Usage:
+  createForm({
+    name: 'test2', enabled: true, email_to: 'test@test8.com', webhooks: [], required_fields: []
+  })
+*/
+export function createForm(data){
+  return fetch('/forms', {
+    credentials: 'include',
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
+
+/* Send server request to update existing form
+
+  Usage:
+  updateForm({
+    name: 'test2', enabled: true, email_to: 'test@test8.com', webhooks: [], required_fields: []
+  })
+*/
+export function updateForm(data){
+  return fetch('/forms/5oFr9Lr', {
+    method: 'put', credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+}
