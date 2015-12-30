012461c3ff9985b1d9dcd100018cbe6b9692b36a
Peter Moresi <peter.moresi@gmail.com>
2015-12-29 18:33:43 -0800

    Add hack to highlight URL on homepage

--- src/components/markdown-options.js
+++ src/components/markdown-options.js
@@ -4,7 +4,12 @@ export default {
   highlight: function (str, lang) {
     if (lang && hljs.getLanguage(lang)) {
       try {
-        return hljs.highlight(lang, str).value;
+        // Custom hack for homepage
+        var tweaked = hljs.highlight(lang, str).value.replace(
+          '<span class="hljs-string">"https://formbucket.com/f/ff4fu3"</span>',
+          '<span class="hljs-string">"<span class="hljs-highlight">https://formbucket.com/f/ff4fu3</span>"</span>' );
+
+        return tweaked;
       } catch (err) {}
     }
 
