09c3a1069ebed4289e36e3d873f9c812a35dde6c
Sean King <sean@functionfoundry.com>
2016-01-01 14:00:34 -0800

    no more tabs on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -46,42 +46,33 @@ const Dashboard = React.createClass({
           </div>
         </div>
         <div className="wrapper">
-          <ul className="accordion-tabs-minimal">
-            <li className="tab-header-and-content">
-              <a href="#" className="tab-link is-active">Active Buckets</a>
-              <div className="tab-content">
-                <div className="callout">
-                  <button onClick={this.handleNewBucket}><FontAwesome name='plus' /> New Bucket</button>
-                  <p>You are using 3 out of 5 available active buckets in <Link to="billing">your plan</Link>.</p>
-                </div>
-
-                { typeof this.state.buckets === 'undefined' ? 'Loading...' : (
-                  <table className="bucket-list">
-                    <thead>
-                      <tr>
-                        <th>Bucket Name</th>
-                        <th># Submissions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        this.state.buckets.map(bucket => (
-                          <tr key={bucket.id}>
-                            <td><Link to={`/buckets/${bucket.id}`}><FontAwesome name='gear' /> {bucket.name}</Link></td>
-                            <td><h3><Link to="#">{bucket.submission_count}</Link></h3></td>
-                          </tr>
-                        ))
-                      }
-                    </tbody>
-                  </table>
-                )
-              }
-            </div>
-          </li>
-          <li className="tab-header-and-content">
-            <a href="#" className="tab-link">Archived Buckets</a>
-          </li>
-        </ul>
+          <div className="callout">
+            <button onClick={this.handleNewBucket}><FontAwesome name='plus' /> New Bucket</button>
+            <p>You are using 3 out of 5 available active buckets in <Link to="billing">your plan</Link>.</p>
+          </div>
+          { typeof this.state.buckets === 'undefined' ? 'Loading...' : (
+            <table className="bucket-list">
+              <thead>
+                <tr>
+                  <th>Bucket Name</th>
+                  <th>Status</th>
+                  <th># Submissions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.buckets.map(bucket => (
+                    <tr key={bucket.id}>
+                      <td><Link to={`/buckets/${bucket.id}`}><FontAwesome name='gear' /> {bucket.name}</Link></td>
+                      <td>{bucket.enabled}</td>
+                      <td><h3><Link to="#">{bucket.submission_count}</Link></h3></td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          )
+        }
       </div>
     </div>
   )
