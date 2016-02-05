6f28d2748cee9825471bfc720fcd867c3ceb1127
Peter Moresi <peter.moresi@gmail.com>
2016-02-04 20:23:21 -0800

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,9 @@ const Footer = React.createClass({
       <p>
         &copy; 2015-2016 FormBucket.com <Link to="faqs">FAQs</Link> | <Link to="api">API</Link> | <Link to="affiliates">Affiliates</Link> | <Link to="support">Support</Link>
       </p>
+      <div id="version">
+        v0.1.0
+      </div>
       </div>
     )
   }
