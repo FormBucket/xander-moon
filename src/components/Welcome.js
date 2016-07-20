60c2c2c461bc352ab449ef357b7b60bd993922f1
Peter Moresi <peter.moresi@gmail.com>
2016-07-19 20:27:43 -0400

    prod changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,40 @@
 import React, { PropTypes } from 'react'
 import Markdown from 'react-remarkable'
 import markdownOptions from './markdown-options'
-import {branch} from 'functionfoundry'
-import UserStore from '../stores/user'
+import {branch, isEmail} from 'functionfoundry'
+
+window.checkHomepageForm = function() {
+    var v = true;
+    var name = document.getElementsByName('name')[0].value
+    var email = document.getElementsByName('email')[0].value
+    var message = document.getElementsByName('message')[0].value
+    
+    if (!name) {
+	v = false;
+	document.getElementsByName('name')[0].style.border = '1px red solid'
+    } else {
+	document.getElementsByName('name')[0].style.border = ''
+    }
+
+    if (!email || !isEmail(email)) {
+	v = false;
+	document.getElementsByName('email')[0].style.border = '1px red solid'
+    } else {
+	document.getElementsByName('email')[0].style.border = ''
+    }
+
+    if (!message) {
+	v = false;
+	document.getElementsByName('message')[0].style.border = '1px red solid'
+    } else {
+    	document.getElementsByName('message')[0].style.border = ''
+    }
+
+    return v;
+} 
 
 var content = `<h3>What's on your mind?</h3>
-<form action="https://api.formbucket.com/f/homepage" method="post">
+<form onsubmit="return checkHomepageForm()" action="https://api.formbucket.com/f/homepage" method="post">
   <input type="text" name="name" placeholder="Your name"/>
   <input type="text" name="email" placeholder="Your email"/>
   <textarea name="message" placeholder="Share your feedback, comments or issues"></textarea>
@@ -13,14 +42,43 @@ var content = `<h3>What's on your mind?</h3>
 </form>`
 
 const Welcome = React.createClass({
+  getInitialState: () => {
+    return {
+      ghostTextLength: 0,
+      ghostText: ''
+    }
+  },
+  componentDidMount () {
+    
+    this.timerId = setInterval( () => {
+
+      this.setState({
+        ghostTextLength: this.state.ghostTextLength+1,
+        ghostMarkup: content.substring(0, this.state.ghostTextLength+1 ),
+        ghostText: '```html\n' + content.substring(0, this.state.ghostTextLength+1 ) + '\n```'
+      })
+
+      if (this.state.ghostTextLength+1 > content.length) {
+        this.setState({
+          ghostText: this.state.ghostText + '\n<span class="blinking-cursor" />'
+        })
+        clearInterval(this.timerId)
+      }
+    }, 20)
+      
+  },
+  componentWillUnmount () {
+    clearInterval(this.timerId)
+  },
   render () {
-    return (
+
+   return (
       <div>
         <div className="hero">
           <div className="wrapper">
             <h1>Endpoints are Just the Beginning</h1>
 	    <h2>Groovy Form Handling and Automation for Static Sites</h2>
-            { branch( UserStore.isUserLoggedIn(),
+            { branch( localStorage.hasOwnProperty('token'),
                     <button onClick={() => this.props.history.push('/buckets')}>Return to your buckets</button>,
                     <button onClick={() => this.props.history.push('/signup')}>Sign up for free</button>
              )}
@@ -28,14 +86,13 @@ const Welcome = React.createClass({
                <div className="editor">
                  <div className="left">
                    <div className="typing">
-                     <Markdown
-                       source={ '```html\n' + content + '```' }
-                       options={ markdownOptions }
-                       />
-
+                    <Markdown
+                      source={ this.state.ghostText }
+                      options={ markdownOptions }
+                    />
                    </div>
                  </div>
-                 <div className="right" dangerouslySetInnerHTML={{__html: content }}>
+                 <div className="right" dangerouslySetInnerHTML={{__html: this.state.ghostMarkup }}>
                  </div>
                </div>
              </div>
