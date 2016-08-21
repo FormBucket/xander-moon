a29f824b8f4ddad2fe81b7e30920dfd74437cdf0
Sean King <sean@functionfoundry.com>
2016-08-20 21:00:55 -0700

    Cleaning up Account Profile page

--- src/components/AccountProfile.js
+++ src/components/AccountProfile.js
@@ -66,43 +66,53 @@ const Account = React.createClass({
         </div>
         <div className="wrapper">
           <div className="half-width">
-            <div className="section">
-              <label htmlFor="fullName">Full Name</label>
-              <input type="text" ref="name" name="displayName" defaultValue={this.state.user.name} placeholder="e.g. Nikola Tesla"/>
-              {/*<label htmlFor="orgName">Company / Org</label>
+            <label htmlFor="fullName">Full Name</label>
+            <input type="text" ref="name" name="displayName" defaultValue={this.state.user.name} placeholder="e.g. Nikola Tesla"/>
+            {/*<label htmlFor="orgName">Company / Org</label>
               <in$put type="text" ref="org" defaultValue={this.state.user.org} />*/}
-              <label htmlFor="emailAddress">Email Address</label>
-              <input type="text" ref="email" name="username" defaultValue={this.state.user.email} placeholder="nikola@altcurrent.com"/>
-              <label htmlFor="password"><FontAwesome name='lock' /> Change Password</label>
-              <input type="password" ref="password" defaultValue="" />
-              <button disabled={this.state.saving} className="button secondary" onClick={this.handleSave}>Save Changes</button>
-            </div>
-            <label>Remove local security token</label>
-            <button className="button secondary" onClick={() => {
-                localStorage.removeItem('token');
-                this.props.history.push('/');
-              } }>Log out</button>
-              <label>Security token <button className="button secondary" onClick={() => this.setState({ show_token: !this.state.show_token })}>{this.state.show_token ? 'hide' : 'show' }</button></label>
-              <textarea rows={4} value={this.state.show_token ? localStorage.token : ''} style={{ display: this.state.show_token ? '' : 'none' }} />
-              <label>Delete my account (cannot be undone)</label>
-              <button onClick={this.handleDeleteAccount} className="button secondary">Destroy Account</button>
-              { /*IF(this.state.active,
-                <div>
-                <hr />
-                <label>Stop billing and unsubscribe from this service</label>
-                <button className="button secondary" onClick={this.handleDelete}>Cancel Subscription</button>
-                </div>,
-                <div>
-                <hr />
-                Subscription is not active. <Link to="account/billing">Activate Subscription</Link>
-                </div>
-                ) */}
-                {/* <hr />
-                <label>Download account archive</label>
-                <button className="button secondary" onClick={() => alert('tbd')}>Download Archive</button> */}
+            <label htmlFor="emailAddress">Email Address</label>
+            <input type="text" ref="email" name="username" defaultValue={this.state.user.email} placeholder="nikola@altcurrent.com"/>
+            <label htmlFor="password"><FontAwesome name='lock' /> Change Password</label>
+            <input type="password" ref="password" defaultValue="" />
+            <button disabled={this.state.saving} className="button secondary" onClick={this.handleSave}>Save Changes</button>
+            { /*IF(this.state.active,
+              <div>
+              <hr />
+              <label>Stop billing and unsubscribe from this service</label>
+              <button className="button secondary" onClick={this.handleDelete}>Cancel Subscription</button>
+              </div>,
+              <div>
+              <hr />
+              Subscription is not active. <Link to="account/billing">Activate Subscription</Link>
               </div>
+              ) */}
+              {/* <hr />
+              <label>Download account archive</label>
+              <button className="button secondary" onClick={() => alert('tbd')}>Download Archive</button> */}
+            </div>
+            <div className="account-sidebar">
+              <p>
+                <a onClick={() => {
+                    localStorage.removeItem('token');
+                    this.props.history.push('/');
+                  } }>Log Out
+                </a>
+              </p>
+              <p>
+                <a onClick={() => this.setState({ show_token: !this.state.show_token })}>{this.state.show_token ? 'Hide API Key' : 'Show API Key' }</a>
+                <div style={{ display: this.state.show_token ? '' : 'none' }}>
+                  <p>
+                    <a href="/docs/api">(How to use this)</a>
+                  </p>
+                  <textarea rows={4} value={this.state.show_token ? localStorage.token : ''} />
+                </div>
+              </p>
+              <p>
+                <a className="danger" onClick={this.handleDeleteAccount}>Destroy Account Forever</a>
+              </p>
             </div>
           </div>
+        </div>
         )
       }
     })
