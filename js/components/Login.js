c87729f62c4529f4a85b779c4791d1dabc37a859
Peter <peter.moresi@gmail.com>
2016-09-07 00:05:40 -0700

    Fix login message

--- js/components/Login.js
+++ js/components/Login.js
@@ -21,7 +21,7 @@ const Login = React.createClass({
       err => {
         this.setState({
           loading: false,
-          error: err
+          error: JSON.parse(err)
         })
       }
     )
