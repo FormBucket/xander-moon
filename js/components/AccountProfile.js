9e5bf57d8b06114bca02794a6fb269d222d6c328
Sean King <sean@websitehq.com>
2016-12-07 06:40:38 -0800

    adding billing to account section

--- js/components/AccountProfile.js
+++ js/components/AccountProfile.js
@@ -3,6 +3,7 @@ import FontAwesome from 'react-fontawesome'
 import {Link} from 'react-router'
 import FlashMessage from './FlashMessage'
 import {requestProfile, requestUpdateUser, requestDestroyAccount} from '../stores/webutils'
+import CreditCardForm from './CreditCardForm'
 
 const Account = React.createClass({
   getInitialState () {
@@ -81,6 +82,7 @@ const Account = React.createClass({
             <input type="text" ref="email" name="username" defaultValue={this.state.user.email} placeholder="nikola@altcurrent.com"/>
             <label htmlFor="password"><FontAwesome name='lock' /> Change Password</label>
             <input type="password" ref="password" defaultValue="" />
+            <CreditCardForm />
             <button disabled={this.state.saving} className="button secondary" onClick={this.handleSave}>Save Changes</button>
             { /*IF(this.state.active,
               <div>
@@ -96,44 +98,44 @@ const Account = React.createClass({
               {/* <hr />
               <label>Download account archive</label>
               <button className="button secondary" onClick={() => alert('tbd')}>Download Archive</button> */}
-            </div>
-            <div className="account-sidebar">
-              <p>
-                <a onClick={() => {
-                    localStorage.removeItem('token');
-                    this.props.history.push('/');
-                  } }>Log Out
-                </a>
-              </p>
-              {/*<p>
-                <a onClick={() => {
-                    this.props.history.push('/upgrade');
-                  } }>Manage Plan
-                </a>
-              </p>*/}
-              <p>
-                <a onClick={() => this.setState({ show_token: !this.state.show_token })}>{this.state.show_token ? 'Hide API Key' : 'Show API Key' }</a>
-                <div style={{ display: this.state.show_token ? '' : 'none' }}>
-                  <p>
-                    <a href="/docs/api">(How to use this)</a>
-                  </p>
-                  <textarea rows={4} value={this.state.show_token ? localStorage.token : ''} />
-                </div>
-              </p>
-              <p>
-                <a onClick={() => {
-                    this.props.history.push('/logs');
-                  } }>View Logs
-                </a>
-              </p>
-              <p>
-                <a className="danger" onClick={this.handleDeleteAccount}>Destroy Account Forever</a>
-              </p>
-            </div>
+          </div>
+          <div className="account-sidebar">
+            <p>
+              <a onClick={() => {
+                  localStorage.removeItem('token');
+                  this.props.history.push('/');
+                } }>Log Out
+              </a>
+            </p>
+            <p>
+              <a onClick={() => {
+                  this.props.history.push('/billing');
+                } }>Billing History
+              </a>
+            </p>
+            <p>
+              <a onClick={() => this.setState({ show_token: !this.state.show_token })}>{this.state.show_token ? 'Hide API Key' : 'Show API Key' }</a>
+              <div style={{ display: this.state.show_token ? '' : 'none' }}>
+                <p>
+                  <a href="/docs/api">(How to use this)</a>
+                </p>
+                <textarea rows={4} value={this.state.show_token ? localStorage.token : ''} />
+              </div>
+            </p>
+            <p>
+              <a onClick={() => {
+                  this.props.history.push('/logs');
+                } }>View Logs
+              </a>
+            </p>
+            <p>
+              <a className="danger" onClick={this.handleDeleteAccount}><FontAwesome name="frown-o" /> Cancel Account</a>
+            </p>
           </div>
         </div>
-        )
-      }
-    })
+      </div>
+      )
+    }
+  })
 
-    export default Account
+  export default Account
