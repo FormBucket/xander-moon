02cda3b896c56babc0febf00ddea952a266d4c8e
Sean King <sean@websitehq.com>
2017-02-08 20:31:26 -0800

    Updating footers with new copyright text

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = React.createClass({
           <img src="/img/logo-purple.svg" alt="FormBucket" />
         </Link>
         <p>
-          &copy; 2015-2017 <a href="https://formbucket.com">FormBucket, LLC</a>
+          &copy; 2015-2017 <a href="https://www.formbucket.com">FormBucket.com</a>
       </p>
       <div>
         <a href="/about">About</a> | <a href="/contact">Contact</a> | <a href="/terms">Terms</a> | <a href="/privacy-policy">Privacy Policy</a>
