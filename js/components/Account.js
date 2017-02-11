a8430a3a2b1026321ad026b067e3153d2ad6307b
Sean King <sean@websitehq.com>
2017-02-10 20:00:09 -0800

    Removing lock icon from Password label

--- js/components/Account.js
+++ js/components/Account.js
@@ -174,7 +174,7 @@ const Account = React.createClass({
               <in$put type="text" ref="org" defaultValue={this.state.user.org} />*/}
             <label htmlFor="emailAddress">Email Address</label>
             <input type="text" ref="email" name="username" defaultValue={this.state.user.email} placeholder="nikola@altcurrent.com"/>
-            <label htmlFor="password"><FontAwesome name='lock' /> Change Password</label>
+            <label htmlFor="password">Change Password</label>
             <input type="password" ref="password" defaultValue="" />
             {
               branch(
