65f2b9b7255539cffc9b1ece126c6415756dd263
FormBucket Support <support@formbucket.com>
2018-02-09 23:24:28 +0000

    Fix contact form

--- js/components/Contact.js
+++ js/components/Contact.js
@@ -9,6 +9,22 @@ import Content from '../contact.md'
 
 let Contact = class extends React.Component {
 
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      if (grecaptcha) {
+        grecaptcha.render('contact-recaptcha', {
+          sitekey: '6Lc_YSgTAAAAAPdIJ5hVuFFNvoljmLYx3E1d6kcu'
+        });
+        clearInterval(this.timerId)
+        console.log('cleared')
+      }
+    }, 200);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timerId)
+  }
+
   render() {
     return (
       <Layout>
